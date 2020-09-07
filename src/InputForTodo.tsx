07420d38c78e6715cb50d8ea5581897395e0d603
Harsh Gorakhia <harsh.gorakhia@gatewaygroup.com>
2020-09-07 20:23:33 +0530

    Updated - Changes - 07/09/2020

--- src/InputForTodo.tsx
+++ src/InputForTodo.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Col, InputGroup, Input, InputGroupAddon, Button, Alert, Row } from 'reactstrap';
 import { connect } from 'react-redux';
 import { ADD_TODO, UPDATE_TODO, todoNodeStructure} from './todoSlice';
+import { Typeahead } from 'react-bootstrap-typeahead';
 import { RootState } from '.';
 
 const InputForTodo = React.memo(function (props : any = {editedTitle : ''}) {
@@ -15,6 +16,7 @@ const InputForTodo = React.memo(function (props : any = {editedTitle : ''}) {
     return (
         <Row>
             <Col xs="4">
+                <Typeahead options={props.todos} labelKey={todo => `${}`}></Typeahead>
                 <InputGroup>
                     <Input type="text" disabled={props.type === "EDIT" ? true : false} id={props.type === "EDIT" ? "edit_todo_input" : "add_todo_input"} placeholder={props.type === "ADD" ? "What do you want to do?" : "" } onKeyPress={(e: any) => e.key === 'Enter' && toDoTitle.length >= 5 ? props.type ==="ADD" ? props.ADD_TODO({id: props.todos.length + 1, title : toDoTitle, completed : false}) && updateToDoTitle('') : props.UPDATE_TODO({id: props.id, title : toDoTitle}) && (e.target.disabled = true) && props.updateSelectedTitle('') && props.updateId(0): console.log(e.key)} value={ toDoTitle } onChange={(e) => { updateToDoTitle(e.target.value); e.target.value.length < 5 ? toggleToDoAlertVisibility(true) : toggleToDoAlertVisibility(false)}}/>
                     {/* <InputGroupAddon addonType="append">{props.type === "ADD" ? <Button color="primary" size="sm" id="todo_add_button" disabled={ toDoTitle.length < 5 ? true : false} onClick={() => { props.ADD_TODO({id: props.todos.length + 1, title : toDoTitle, completed : false}); updateToDoTitle('') }}>Add</Button>: <Button color="primary" size="sm" id="edit_todo_button" disabled={toDoTitle.length < 5 || props.todos.find((todo : todoNodeStructure) => todo.id === props.id)?.title === toDoTitle ? true : false} onClick={() => {props.UPDATE_TODO({id: props.id, title : toDoTitle});  (document.getElementById('edit_todo_button') as HTMLButtonElement).disabled = true; props.updateSelectedTitle(''); props.updateId(0)}}>Save</Button>}</InputGroupAddon> */}
